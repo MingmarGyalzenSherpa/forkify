81229901cd7ea9e16f772344b0a2d292ce97ac0e
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-11-07 20:27:28 +0545

    pagination half complete

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -41,11 +42,16 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     //3. Render results
-    resultsView.render(model.getSearchResultsPage(1));
+    resultsView.render(model.getSearchResultsPage());
+
+    //4. Render initial pagination button
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
+
+const controlPagination = function () {};
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
