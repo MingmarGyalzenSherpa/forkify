824aa20d0ac02020fa23f0150d2438d940322521
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2022-11-11 22:56:52 +0545

    implemented servings change:

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,12 +60,20 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  //update the recipe servings (in state)
+  model.updateServings(newServings);
+  //update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandler(controlPagination);
 };
 init();
-controlSearchResults();
+
 // window.addEventListener('hashchange', showRecipe);
 // window.addEventListener('load', showRecipe);
